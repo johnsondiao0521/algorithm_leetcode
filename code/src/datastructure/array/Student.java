479f7674d508a4e1d39922500a5b547997fe38e0
johnsondiao0521 <johnsondiao0521@gmail.com>
2021-07-28 15:17:42 +0800

    提交student类型的数组测试

--- code/src/datastructure/array/Student.java
+++ code/src/datastructure/array/Student.java
@@ -1,4 +1,26 @@
 package datastructure.array;
 
 public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("datastructure.Student(name: %s, score: %d) \n", name, score);
+    }
+
+    public static void main(String[] args) {
+
+        Array<Student> arr = new Array<>();
+        arr.addLast(new Student("Alice", 100));
+        arr.addLast(new Student("Bob", 66));
+        arr.addFirst(new Student("Charlie", 88));
+        System.out.println(arr);
+    }
 }
