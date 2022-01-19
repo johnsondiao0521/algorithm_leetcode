8a59e4f6823a915c987e3d1f773bb09d84984b8c
diaozhida <332886494@qq.com>
2022-01-19 22:42:34 +0800

    二分搜索树的前中序遍历

--- code/src/datastructure/bst/BST.java
+++ code/src/datastructure/bst/BST.java
@@ -45,6 +45,94 @@ public class BST<E extends Comparable<E>> {
             node.right = add(node.right, e);
 
         return node;
+    }
 
+    //二分搜索树中是否包含元素e
+    public boolean contains(E e){
+        return contains(root, e);
+    }
+
+    // 向以node为根的二分搜索树中是否包含元素e，递归算法
+    private boolean contains(Node node, E e){
+
+        if(node == null)
+            return false;
+        if(e.compareTo(node.e) == 0)
+            return true;
+        else if(e.compareTo(node.e) < 0)
+            return contains(node.left, e);
+        else
+            return contains(node.right, e);
+    }
+
+    // 二分搜索树的前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    //前序遍历以node为根的二分搜索树，递归算法
+    private void preOrder(Node node){
+        if(node == null)
+            return;
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    // 二分搜索树的中序遍历
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    //中序遍历以node为根的二分搜索树，递归算法
+    private void inOrder(Node node){
+
+        if (node == null)
+            return;
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+
+    }
+
+    // 二分搜索树的后序遍历
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    //后序遍历以node为根的二分搜索树，递归算法
+    private void postOrder(Node node){
+
+        if (node == null)
+            return;
+        inOrder(node.left);
+        inOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    //生成以node为根节点
+    private void generateBSTString(Node node, int depth, StringBuilder res){
+        if(node == null){
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i<depth; i++)
+            res.append("--");
+        return res.toString();
     }
 }
